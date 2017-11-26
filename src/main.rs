d1652e2d8bc3a8e454e21430b50a5da9e1fe0010
Hidekazu Kobayashi <kobahide789@gmail.com>
2017-11-27 04:14:03 +0900

    Remove needless(?) `extern "C"`

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ extern crate serde_json;
 fn main() {}
 
 #[no_mangle]
-pub extern "C" fn alloc(size: usize) -> *mut c_void {
+pub fn alloc(size: usize) -> *mut c_void {
     let mut buf = Vec::with_capacity(size);
     let ptr = buf.as_mut_ptr();
     mem::forget(buf);
@@ -18,7 +18,7 @@ pub extern "C" fn alloc(size: usize) -> *mut c_void {
 }
 
 #[no_mangle]
-pub extern "C" fn dealloc(ptr: *mut c_void, cap: usize) {
+pub fn dealloc(ptr: *mut c_void, cap: usize) {
     unsafe  {
         let _buf = Vec::from_raw_parts(ptr, 0, cap);
     }
